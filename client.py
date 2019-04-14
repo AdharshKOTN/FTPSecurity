62e0be2b99c3c07827cc74e5bac88454b5b155d9
Adharsh Rajendran <adharsh.rajendran@gmail.com>
2019-04-14 00:05:20 -0500

    fixing client connection

--- client.py
+++ client.py
@@ -9,14 +9,14 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
 	#create socket object
     s.connect((HOST, PORT))	#connect to server
-    print('Connecting to ' + HOST + ' on Port ' + PORT)
-    #s.sendall(b'Hello, world')	#send message, exchange with code to obtain file
-								#client ui should loop here and access files
-    #data = s.recv(1024)	#read server reply
-    s.close()
 except:
     print('Connection error')
 
+print('Connecting to ' + HOST + ' on Port ' + PORT)
+    #s.sendall(b'Hello, world')	#send message, exchange with code to obtain file
+								#client ui should loop here and access files
+    #data = s.recv(1024)	#read server reply
+s.close()
 print('Client should be closed now.')
 
 #print('Received', repr(data))	#print server reply
