e475c199f67f373ca87f0014e084b8c978c52edd
Adharsh Rajendran <adharsh.rajendran@gmail.com>
2019-04-16 21:19:38 -0500

    client user input

--- client.py
+++ client.py
@@ -10,9 +10,14 @@ try:
 	#create socket object
     s.connect((HOST, PORT))	#connect to server
 except:
-    print('Connection error')
+    print('Connection error, check the host or port number')
+	break
 
 print('Connecting to ' + str(HOST) + ' on Port ' + str(PORT))
+while true:
+	cmnd = input("Command:")
+	if(cmnd == 'exit'):
+		break
     #s.sendall(b'Hello, world')	#send message, exchange with code to obtain file
 								#client ui should loop here and access files
     #data = s.recv(1024)	#read server reply
