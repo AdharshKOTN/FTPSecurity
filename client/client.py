a4e93d56c3ed2415ae7267c3c91c882e15c676bc
Adharsh <adstorm14@gmail.com>
2019-04-24 00:02:43 -0500

    finalized functionality, must clean up

--- client/client.py
+++ client/client.py
@@ -2,8 +2,8 @@
 import socket
 import sys
 import getpass
-from Crypto.Cipher import DES
-from Crypto import Random
+from simplecrypt import encrypt, decrypt
+import time
 
 HOST = ''
 PORT = 63430 #number greater than 1023 and less than 65536
@@ -16,6 +16,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #	_auth_count = val
 #def _getAuthCount()
 #	return _auth_count
+class enc:
+	def __init__(self, val):
+		self.__encryption_pass = val
+	def setEncryptPass(self, val):
+		self.__encryption_pass = val
+	def getEncryptPass(self):
+		return self.__encryption_pass
 
 try:
 	#create socket object
@@ -31,23 +38,29 @@ print('Connecting to ' + str(HOST) + ' on Port ' + str(PORT))
 username = input("Username: ")
 password = getpass.getpass(prompt = 'Password: ')
 userDetails = username + "," + password
-
-#iv = Random.get_random_bytes(8)
-#des1 = DES.new(userDetails, DES.MODE_CFB, iv)
-#cyper_ud = des1.encrypt(userDetails)
-
-#message = iv + "," + cypher_ud
-
-#print(str(message))
+enc1 = enc('temp')
 
 while 1:
-	#s.send(message.encode())
+	enc1.setEncryptPass(getpass.getpass(prompt='Encryption Pass: '))
+	#print('Encrypted Pass: ' + enc1.getEncryptPass() + p)
+	s.send(enc1.getEncryptPass().encode())
+	time.sleep(1)
+	#print('enc pass sent')
 	s.send(userDetails.encode())
+	#print('user details sent')
+	encAuth = s.recv(1024).decode()
+	#print('recieved enc auth code')
+	if(encAuth != 'pass'):
+		print('Encryption passcode incorrect')
+		continue
+	print('Encryption Pass Accepted')
+	#cipher_text = encrypt(_getEncryptPass(),userDetails)
+	#s.send(cipher_text.encode())
 	print("User Details: " + str(userDetails) + " has been sent.")
 	authentication_result = s.recv(1024).decode()
 	if(authentication_result != "pass"):
 		print('Authentication Failed...')
-		#_setAuthCount(val - 1)
+		
 		username = input("Username: ")
 		password = getpass.getpass(prompt = 'Password: ')
 		userDetails = username + "," + password
@@ -58,7 +71,7 @@ while 1:
 while 1:
 	cmnd = input("Exit | Send File | Access File:\n")
 	cmnd = cmnd.lower()
-	#print(cmnd)
+	
 	if (cmnd == 'exit'):
 		print('Exiting Client Application')
 		client_exit = 'CEX000'
@@ -72,43 +85,53 @@ while 1:
 		while 1:
 			filename = input('Provide File Name: ')
 			try:
-				file = open(filename, 'rb')
+				file = open(filename, 'r')
 				break
 			except:
 				print('File does not exist.')
-		#print('Filename has been sent to the server.')
-		#s.send(file)
-		file_data = file.readline()
-		while (file_data):
-			s.send(file_data)
-			print('Sent ',repr(file_data))
-			file_data = file.readline()
-		eof = 'CEOF004'
-		s.send(eof.encode())
+		
+		print('Sending File Name: ' + str(filename))
+		s.send(filename.encode())
+
+		file_data = file.read()
+		file_data = encrypt(enc1.getEncryptPass(), file_data)
+		s.send(file_data)
+		
 		file.close()
 		print('The file has finished sending')
 	elif (cmnd == 'access file'):
 		access_file = 'CAF002'
 		s.send(access_file.encode())
 		while 1:
-			filename = input('Provide File Name: ')
-			s.send(filename.encode())
+			file_name = input('Provide File Name: ')
+			s.send(file_name.encode())
 			file_status = s.recv(1024).decode()
+			print(file_status)
 			if(file_status == 'File does not exist.'):
 				print('File does not exist.')
 			else:
 				break
-		print('The filename has been sent')
-		file_data = s.recv(1024).decode()
-		file = open(filename, "a")
-		while 1:
-			print('Recieved: ' + file_data)
-			if (file_data == 'SEOF004'):
-				#print('DEBUG: exit the inner loop')
-				break
-			else:
-				file_data = s.recv(1024).decode()
-				file.write(file_data)
+
+		print('Recieving File...')
+
+		try:
+			#file_name = str(connection.recv(1024).decode("utf-8"))
+			print("Server will send this file: " + file_name)
+			file = open(file_name, "w")
+		except:
+			print('Unable to write to the file.')
+		
+		try:
+			file_data = s.recv(1024)
+			file_data = decrypt(enc1.getEncryptPass(), file_data).decode("utf-8")
+		except:
+			print("Unable to recieve/decrypt file")
+
+		print("this is from the file: " + str(file_data))
+		file.write(file_data)
+		print('File has been created')
+		file.close()
+		
 	elif (cmnd == 'secret close'):
 		secret = 'SECRCL005'
 		s.send(secret.encode())
--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@
 import socket
 import select
 import sys
-
+from simplecrypt import encrypt, decrypt
 
 HOST = ''
 PORT = 63430 #number greater than 1023 and less than 65536
@@ -15,12 +15,25 @@ _passwords = {
 	"user1":"pass1"
 }
 
+_encpass = {
+	"admin":"password1",
+	"user1":"password2"
+}
+
 def _confirmAccess(username, password):
 	if(_passwords.get(username) == password):
 		print('UserAuth Success')
 		return True
 	else:
 		print('UserAuth Fail')
+		return False
+
+def _confirmEncryption(username, encPass):
+	if(_encpass.get(username) == encPass):
+		print('Encryption Auth Success')
+		return True
+	else:
+		print('Encryption Auth Fail')
 		return False
 		
 try:
@@ -37,11 +50,20 @@ while 1:
 	connection, addr = s.accept() #waits for connection from client
 	#returns socket object for connection and client address
 	print('Connected by: ' + str(addr))
-	userDetails = connection.recv(1024).decode()
 	while 1:
+		userEncPass = connection.recv(1024).decode()
+		print(userEncPass)
+		userDetails = connection.recv(1024).decode()
+		print(userDetails)
 		userDetailsArr = userDetails.split(",")
 		username = userDetailsArr[0]
 		password = userDetailsArr[1]
+		if(_confirmEncryption(username, userEncPass) == False):
+			connection.send('fail'.encode())
+			continue
+		else:
+			connection.send('pass'.encode())
+		
 		print('Obtained details from user: ' + str(username) + ' password provided: ' + str(password))
 		if(_confirmAccess(username,password) == False):
 			print('User Authentication Failed')
@@ -68,44 +90,46 @@ while 1:
 			elif (str(client_response) == 'CSF001'):
 				print('Client is sending a file')
 				print('Recieving File...')
-				file_name = connection.recv(1024).decode()
-				file = open(file_name, "a")
-				file_data = connection.recv(1024).decode()
-				while 1:
-					print('Recieved: ' + file_data)
-					if (file_data == 'CEOF004'):
-						#print('DEBUG: exit the inner loop')
-						break
-					else:
-						file_data = connection.recv(1024).decode()
-						file.write(file_data)
-						#print('Data from file: ' + file_data)
+
+				try:
+					file_name = str(connection.recv(1024).decode("utf-8"))
+					print("Server will write to this file: " +file_name)
+					file = open(file_name, "w")
+				except:
+					print('Unable to recieve the file name.')
+
+				s.shutdown(socket.SHUT_WR)
+				file_data = connection.recv(1024)
+				file_data = decrypt(_encpass.get(username), file_data).decode("utf-8")
+				print("this is from the file: " + file_data)
+				file.write(file_data)
+				
 			elif (str(client_response) == 'CAF002'):
 				print('Sending File...')
-				#check if the file exists on the server
-				#if it doesn't let the client know
+				
 				file_name = connection.recv(1024).decode()
 				while 1:
-					print('Server will send the file called: ' + str(file_name))
+					print('Server will find the file called: ' + str(file_name))
 					try:
-						file = open(file_name, 'rb')
+						file = open(file_name, 'r')
 						break
 					except:
 						file_does_not_exist = 'File does not exist.'
 						print('Client is attempting to access file that is not available.')
 						connection.send(file_does_not_exist.encode())
 						file_name = connection.recv(1024).decode()
+				file_does_exist = 'File does exist.'
+				connection.send(file_does_exist.encode())
 				#open the file on the server and send the data to the client
 				print('File: ' + file_name + ' has been opened on the server.')
-				file_data = file.readline()
-				while (file_data):
-					connection.send(file_data)
-					print('Sent ',repr(file_data))
-					file_data = file.readline()
+				file_data = file.read()
+				file_data = encrypt(_encpass.get(username), file_data)
+				#print(file_data)
+				connection.send(file_data)
+				
 				file.close()
 				print('The file has finished sending')
-				eof = 'SEOF004'
-				connection.send(eof.encode())
+				
 			elif (str(client_response) == 'SECRCL005'):
 				print('Secret Command Activated. Turning server off.')
 				sys.exit()
